9a98aa06ee76ce40fe1c77ec9f8c6f8a7658caa8
Harrishen <408641204@qq.com>
2019-06-05 18:25:32 +0900

    Initial commit

---
+++ Flash Chat/ChatViewController.swift
@@ -0,0 +1,193 @@
+//
+//  ViewController.swift
+//  Flash Chat
+//
+//  Created by Angela Yu on 29/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import ChameleonFramework
+
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+
+    
+    // Declare instance variables here
+    var messageArray: [Message] = [Message]()
+    
+    // We've pre-linked the IBOutlets
+    @IBOutlet var heightConstraint: NSLayoutConstraint!
+    @IBOutlet var sendButton: UIButton!
+    @IBOutlet var messageTextfield: UITextField!
+    @IBOutlet var messageTableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //TODO: Set yourself as the delegate and datasource here:
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
+
+        
+        //TODO: Set yourself as the delegate of the text field here:
+        messageTextfield.delegate = self
+
+        
+        
+        //TODO: Set the tapGesture here:
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
+        messageTableView.addGestureRecognizer(tapGesture)
+
+        //TODO: Register your MessageCell.xib file here:
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        configureTabelView()
+        retrieveMessages()
+        
+        //get rid of the seperate lines
+        messageTableView.separatorStyle = .none
+    }
+
+    ///////////////////////////////////////////
+    
+    //MARK: - TableView DataSource Methods
+    
+    
+    
+    //TODO: Declare cellForRowAtIndexPath here:
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+        cell.messageBody.text = messageArray[indexPath.row].messageBody
+        cell.senderUsername.text = messageArray[indexPath.row].sender
+        cell.avatarImageView.image = UIImage(named: "egg")
+        if cell.senderUsername.text == Auth.auth().currentUser?.email as String! {
+            cell.avatarImageView.backgroundColor = UIColor.flatMint()
+            cell.messageBackground.backgroundColor = UIColor.flatSkyBlue()
+            
+        } else {
+            cell.avatarImageView.backgroundColor = UIColor.flatWatermelon()
+            cell.messageBackground.backgroundColor = UIColor.flatGray()
+        }
+        return cell
+    }
+    
+    
+    
+    //TODO: Declare numberOfRowsInSection here:
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messageArray.count
+    }
+    
+    
+    //TODO: Declare tableViewTapped here:
+    @objc func tableViewTapped () {
+        messageTextfield.endEditing(true)
+    }
+    
+    
+    //TODO: Declare configureTableView here:
+    func configureTabelView() {
+        messageTableView.rowHeight = UITableView.automaticDimension
+        messageTableView.estimatedRowHeight = 120.0
+    }
+    
+    
+    ///////////////////////////////////////////
+    
+    //MARK:- TextField Delegate Methods
+    
+    
+
+    
+    //TODO: Declare textFieldDidBeginEditing here:
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.2) {
+            self.heightConstraint.constant = 317
+            //if something changed, update UI
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    
+    
+    //TODO: Declare textFieldDidEndEditing here:
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.2) {
+            self.heightConstraint.constant = 50
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    
+    ///////////////////////////////////////////
+    
+    
+    //MARK: - Send & Recieve from Firebase
+    
+    
+    
+    
+    
+    @IBAction func sendPressed(_ sender: AnyObject) {
+        
+        
+        //TODO: Send the message to Firebase and save it in our database
+        messageTextfield.endEditing(true)
+        messageTextfield.isEnabled = false
+        sendButton.isEnabled = false
+        
+        let messagesDB = Database.database().reference().child("Messages")
+        let messageDict = ["Sender": Auth.auth().currentUser?.email, "MessageBody": messageTextfield.text!]
+        
+        messagesDB.childByAutoId().setValue(messageDict) { (error, reference) in
+            if error != nil {
+                print(error!)
+            } else {
+                print("Message sent!")
+                self.messageTextfield.isEnabled = true
+                self.sendButton.isEnabled = true
+                self.messageTextfield.text = ""
+            }
+        }
+    }
+    
+    //TODO: Create the retrieveMessages method here:
+    func retrieveMessages() {
+        let messagesDB = Database.database().reference().child("Messages")
+        messagesDB.observe(.childAdded) { (snapshot) in
+            let snapshotValue = snapshot.value as! Dictionary<String, String>
+            let message = snapshotValue["MessageBody"]!
+            let sender = snapshotValue["Sender"]!
+            let newMessage = Message()
+            newMessage.messageBody = message
+            newMessage.sender = sender
+            self.messageArray.append(newMessage)
+            self.configureTabelView()
+            self.messageTableView.reloadData()
+        }
+    }
+    
+
+    
+    
+    
+    @IBAction func logOutPressed(_ sender: AnyObject) {
+        
+        //TODO: Log out the user and send them back to WelcomeViewController
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        }
+        catch {
+            print("Error: sign out failure")
+            //do something to the UI
+            
+        }
+        
+    }
+    
+
+
+}
